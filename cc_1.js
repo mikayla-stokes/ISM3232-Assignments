a20fba54aab5d83f18009356c25fde319d8f86cf
mikayla-stokes <mikaylastokes@usf.edu>
2024-05-15 23:20:57 -0400

    Add output functionality to display bill, tip, and total.

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,5 @@
+//Calculate the Tip
 let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+
+// Output Details
+console.log(`The bill was $${bill.toFixed(2)}, the tip was $${tip.toFixed(2)}, and the total value $${(bill + tip).toFixed(2)}`);
