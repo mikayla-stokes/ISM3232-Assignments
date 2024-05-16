123c50e21650663f62cc3af13406fda0d4b00205
mikayla-stokes <mikaylastokes@usf.edu>
2024-05-15 23:32:27 -0400

    Create and test calcTip function.

--- cc_1.js
+++ cc_1.js
@@ -1,5 +1,12 @@
-//Calculate the Tip
-let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+//calcTip function
+function calcTip(bill) {
+  return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+}
+
+// Example
+const bill = 100;
+const tip = calcTip(bill);
+console.log("Tip", tip.toFixed(2));
 
 // Output Details
-console.log(`The bill was $${bill.toFixed(2)}, the tip was $${tip.toFixed(2)}, and the total value $${(bill + tip).toFixed(2)}`);
+console.log(`The bill was $${bill.toFixed(2)}, the tip was $${tip.toFixed(2)}, and the total value $${(bill + tip).toFixed(2)}.`);
